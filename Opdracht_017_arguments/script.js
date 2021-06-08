51225a43867a3cf488bb99a3a9268baa64502c6e
MiekeNap <84043094+MiekeNap@users.noreply.github.com>
2021-06-08 16:52:02 +0200

    Add files via upload

---
+++ Opdracht_017_arguments/script.js
@@ -0,0 +1,14 @@
+/*const paintWalls = function(color){
+        console.log("the wall has been painted " + color);
+};
+
+paintWalls("green")*/
+
+const paintWalls = function(whichWall, color){
+    console.log("the " + whichWall + " wall has been painted " + color);
+};
+const whichWall = "north";
+const color = "orange";
+
+
+paintWalls(whichWall, color);
